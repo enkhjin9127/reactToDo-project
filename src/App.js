d6cc043ccebafae46b865a9bc310e0eac80ff5ac
enkhjin9127 <ka0enkhjin@gmail.com>
2025-01-03 12:25:00 +0800

    started add func

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddButton = () => {
+    setTodo([...todo, inputValue]);
+    setInputValue("");
+  };
+
+  console.log("this is to do", todo);
+
   return (
-    <div className="App center">
+    <div className="App">
       <div className="App-container">
-        <h1>To-Do list</h1>
-        <div className="addTaskContainer">
-          <input className="inputBox" placeholder="Add a new task..."></input>
-          <button className="addButton">Add</button>
+        <div className="center column">
+          <h2>To-Do list</h2>
+          <div className="addTaskContainer">
+            <input
+              onChange={handleInputChange}
+              className="inputBox"
+              placeholder="Add a new task..."
+            ></input>
+            <button onClick={handleAddButton} className="addButton">
+              Add
+            </button>
+            {todo.map((todo) => {
+              return <div>{todo}</div>;
+            })}
+          </div>
         </div>
         <div className="filterButtons">
           <button className="allTaskButton">All</button>
           <button className="activeTaskButton">Active</button>
           <button className="completedTaskButton">Completed</button>
         </div>
+        <div className="center">No tasks yet. Add one above!</div>
       </div>
     </div>
   );
 }
 
 export default App;
+// 11:14
